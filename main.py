db1c2cf46edd12ab5c32f4ace4ca645337ca2017
braden <leachbr787@northwested.org>
2024-12-06 14:16:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,44 @@ import random
 
 #----Number Guess Game----#
 
-name = input('Hello what is your name? ')
-number = random.randint(1,100)
-print(f'\nHi {name} I\'m thinking of a number between 1 and 100.')
-guesses_taken = 0
+# name = input('Hello what is your name? ')
+# number = random.randint(1,100)
+# print(f'\nHi {name} I\'m thinking of a number between 1 and 100.')
+# guesses_taken = 0
 
-while guesses_taken < 5:
-    guess = input('Enter a guess: ')
-    guess = int(guess)
-    guesses_taken += 1   
-    if guess < number:
-        print('\nThat was too low.')
-    elif guess > number:
-        print('\nThat was too high!')
-    else: 
-        break
-if guess == number: 
-    print(f'Winner winner, chicken dinner! Congrats {name} you guessed the correct number!')
-else: 
-    print(f'you lose, too bad. Better luck next time. The right number was {number}')
+# while guesses_taken < 5:
+#     guess = input('Enter a guess: ')
+#     guess = int(guess)
+#     guesses_taken += 1   
+#     if guess < number:
+#         print('\nThat was too low.')
+#     elif guess > number:
+#         print('\nThat was too high!')
+#     else: 
+#         break
+# if guess == number: 
+#     print(f'Winner winner, chicken dinner! Congrats {name} you guessed the correct number!')
+# else: 
+#     print(f'you lose, too bad. Better luck next time. The right number was {number}')
 
 #----Managing a list----#
 temperatures = []
-input = 0
-while input != -9999:
-    guesst = input('Enter a temperature in Fahrenheit (-9999 to quit): ')
-    guesst = int(guesst)
-    temperatures += guesst
+number = 0
+
+while number != -9999:
+    number = int(input('Enter a temperature in Fahrenheit (-9999 to quit): '))
+    temperatures.append(number)
     
-    if guesst
+list_length = len(temperatures)
+
+   
+
+if list_length > 0:
+
+    temperatures.pop()
+    print(f'Temeratures entered: {temperatures}')
+    average = sum(temperatures) / len(temperatures)
+    print(f'average temperature:{average:.2f}°F')
+
+# elif list_length < 0: 
+#   average = sum(0) / len(0)
